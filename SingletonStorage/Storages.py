0998e678254481992a5db6c2e470df0b15251d17
koki.chin <545705507@qq.com>
2024-06-14 10:15:46 +0900

    add hot backend switching

--- SingletonStorage/Storages.py
+++ SingletonStorage/Storages.py
@@ -1,161 +1,202 @@
 
 import fnmatch
 import json
-import os
 from urllib.parse import urlparse
 
 def get_error(func):
     try:
         func()
-        return None
     except Exception as e:
         return e
 
 # self checking
 es = []
 es.append({get_error(lambda:__import__('google.cloud.firestore'))       :'no google firestore support'})
-es.append({get_error(lambda:os.environ['GOOGLE_PROJECT_ID'])            :'no google firestore support'})
-es.append({get_error(lambda:os.environ['GOOGLE_FIRESTORE_COLLECTION'])  :'no google firestore support'})
 firestore_back = len(list(filter(lambda x:None not in x,es))) == 0
 
 es.append({get_error(lambda:__import__('redis'))                        :'no redis support'})
-es.append({get_error(lambda:os.environ['REDIS_URL'])                    :'no redis url (redis://127.0.0.1:6379)'})
 redis_back = len(list(filter(lambda x:None not in x,es))) == 0
 
 if len(list(filter(lambda x:None not in x,es))) != 0:
     for i in list(filter(lambda x:None not in x,es)):print(i)
 
 
+class SingletonStorageController:
+    def __init__(self, model):
+        self.model = model
+
+    def add_slave(self, slave):
+        self.model.slaves.append(slave)
+        
+    def _set_slaves(self, key: str, value: dict):
+        [s.set(key, value) for s in self.model.slaves if hasattr(s, 'set')]
+    
+    def _delete_slaves(self, key: str):
+        [s.delete(key) for s in self.model.slaves if hasattr(s, 'delete')]
+
+    def exists(self, key: str):
+        print('not implement')
+
+    def set(self, key: str, value: dict):
+        print('not implement')
+
+    def get(self, key: str) -> dict:
+        print('not implement')
+
+    def delete(self, key: str):
+        print('not implement')
+
+    def keys(self, pattern: str):
+        print('not implement')
+
+    def dump(self, json_path=None):
+        print('not implement')
+
+    def load(self, json_path=None):
+        print('not implement')
+
 if firestore_back:
     from google.cloud import firestore
-    os.environ['GOOGLE_PROJECT_ID']
-    os.environ['GOOGLE_FIRESTORE_COLLECTION']
-
     class SingletonFirestoreStorage:
         _instance = None
-        client = firestore.Client(project=os.environ['GOOGLE_PROJECT_ID'])
-        def __new__(cls):
+        _meta = {}
+
+        @staticmethod
+        def _set_instance(cls,google_project_id,google_firestore_collection):
+            cls._instance = super(SingletonFirestoreStorage, cls).__new__(cls)
+            cls._instance.client = firestore.Client(project=google_project_id)
+            cls._instance.collection = cls._instance.client.collection(google_firestore_collection)
+            cls._instance.slaves = []
+            cls._meta['google_project_id']=google_project_id
+            cls._meta['google_firestore_collection']=google_firestore_collection
+        
+        def __new__(cls,google_project_id:str=None,google_firestore_collection:str=None):
             if cls._instance is None:
-                cls._instance = super(SingletonFirestoreStorage, cls).__new__(cls)
+                if google_project_id is None or google_firestore_collection is None:
+                    raise ValueError('google_project_id and google_firestore_collection must not be None at first time')
+                SingletonFirestoreStorage._set_instance(cls,google_project_id,google_firestore_collection)
+            
+            elif ( google_project_id is not None or google_firestore_collection is not None 
+                  ) and (
+                 cls._meta.get('google_project_id',None)!=google_project_id or cls._meta.get('google_firestore_collection',None)!=google_firestore_collection ):
+                print(f'warnning: instance changed to {google_project_id} , {google_firestore_collection}')
+                SingletonFirestoreStorage._set_instance(cls,google_project_id,google_firestore_collection)
             return cls._instance
-        def __init__(self):
-            self.slaves = []
-            self.collection = self.client.collection(os.environ['GOOGLE_FIRESTORE_COLLECTION'])
-    
-    class SingletonFirestoreStorageController:
+        
+        def __init__(self,google_project_id:str=None,google_firestore_collection:str=None):
+            self.slaves:list = self.slaves
+            self.client:firestore.Client = self.client    
+            self.collection:firestore.CollectionReference = self.collection
+    class SingletonFirestoreStorageController(SingletonStorageController):
         def __init__(self, model: SingletonFirestoreStorage):
             self.model = model
-        def add_slave(self, s):
-            self.model.slaves.append(s)
+
         def exists(self, key: str):
             doc = self.model.collection.document(key).get()
             return doc.exists
+        
         def set(self, key: str, value: dict):
             self.model.collection.document(key).set(value)
-            for s in self.model.slaves:
-                if hasattr(s, 'set'):
-                    s.set(key, value)
+            self._set_slaves(key,value)
+
         def get(self, key: str) -> dict:
             doc = self.model.collection.document(key).get()
             return doc.to_dict() if doc.exists else None
+        
         def delete(self, key: str):
             self.model.collection.document(key).delete()
-            for s in self.model.slaves:
-                if hasattr(s, 'delete'):
-                    s.delete(key)
+            self._delete_slaves(key)
+
         def keys(self, pattern: str):
             docs = self.model.collection.stream()
             keys = [doc.id for doc in docs]
             return fnmatch.filter(keys, pattern)
 
-
 if redis_back:
     import redis
-    os.environ['REDIS_URL'] # 'redis://127.0.0.1:6379'
-
     class SingletonRedisStorage:
         _instance = None
+        _meta = {}
 
-        def __new__(cls, *args, **kwargs):
+        @staticmethod
+        def _set_instance(cls,redis_URL):# 'redis://127.0.0.1:6379'
+            url = urlparse(redis_URL)
+            cls._instance = super(SingletonRedisStorage, cls).__new__(cls)                                
+            cls._instance.client = redis.Redis(host=url.hostname, port=url.port, db=0, decode_responses=True)
+            cls._instance.slaves = []
+            cls._meta['redis_URL'] = redis_URL
+
+        def __new__(cls, redis_URL=None):
             if cls._instance is None:
-                cls._instance = super(SingletonRedisStorage, cls).__new__(cls)                
-                url = urlparse(os.environ['REDIS_URL'])
-                cls._instance.redis = redis.Redis(host=url.hostname, port=url.port, db=0, decode_responses=True)
+                if redis_URL is None: raise ValueError('redis_URL must not be None at first time (redis://127.0.0.1:6379)')
+                SingletonRedisStorage._set_instance(cls,redis_URL)
+            elif redis_URL is not None and cls._meta.get('redis_URL',None)!=redis_URL:
+                print(f'warnning: instance changed to url {redis_URL}')
+                SingletonRedisStorage._set_instance(cls,redis_URL)
             return cls._instance
 
-        def __init__(self):
-            self.slaves = []
-
-    class SingletonRedisStorageController:
+        def __init__(self, redis_URL=None):
+            self.slaves:list = self.slaves
+            self.client:redis.Redis = self.client
+    class SingletonRedisStorageController(SingletonStorageController):
         def __init__(self, model: SingletonRedisStorage):
             self.model = model
 
-        def add_slave(self, s):
-            self.model.slaves.append(s)
-
         def exists(self, key: str):
-            return self.model.redis.exists(key)
+            return self.model.client.exists(key)
 
         def set(self, key: str, value: dict):
-            self.model.redis.set(key, json.dumps(value))
-            for s in self.model.slaves:
-                if hasattr(s, 'set'):
-                    s.set(key, value)
+            self.model.client.set(key, json.dumps(value))
+            self._set_slaves(key,value)
 
         def get(self, key: str) -> dict:
-            return json.loads(self.model.redis.get(key))
+            return json.loads(self.model.client.get(key))
 
         def delete(self, key: str):
-            self.model.redis.delete(key)
-            for s in self.model.slaves:
-                if hasattr(s, 'delete'):
-                    s.delete(key)
+            self.model.client.delete(key)
+            self._delete_slaves(key)
+
 
         def keys(self, pattern: str = "*"):
-            return self.model.redis.keys(pattern)
+            return self.model.client.keys(pattern)
 
         def dump(self, path="RedisStorage.json"):
-            all_keys = self.model.redis.keys()
-            all_data = {key: self.model.redis.get(key) for key in all_keys}
+            all_keys = self.model.client.keys()
+            all_data = {key: self.model.client.get(key) for key in all_keys}
             with open(path, "w") as tf:
                 json.dump(all_data, tf)
 
         def load(self, path="RedisStorage.json"):
             with open(path, "r") as tf:
-                data = json.load(tf)
+                data:dict = json.load(tf)
             for key, value in data.items():
-                self.model.redis.set(key, value)
+                self.model.client.set(key, value)
 
 class SingletonPythonDictStorage:
     _instance = None
-    store = {}
-
+    _meta = {}
+    
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(SingletonPythonDictStorage, cls).__new__(cls)
+            cls._instance.store = {}
+            cls._instance.slaves = []
         return cls._instance
-
+    
     def __init__(self):
-        self.slaves = []
+        self.slaves:list = self.slaves
+        self.store:dict = self.store
 
-class SingletonPythonDictStorageController:
+class SingletonPythonDictStorageController(SingletonStorageController):
     def __init__(self, model:SingletonPythonDictStorage):
         self.model = model
-
-    def _get_store(self):
-        return self.model.store
-
-    def add_slave(self, s):
-        self.model.slaves.append(s)
 
     def exists(self, key: str):
         return key in self.model.store
 
     def set(self, key: str, value: dict):
         self.model.store[key] = value
-        for s in self.model.slaves:
-            if hasattr(s, 'set'):
-                s.set(key, value)
+        self._set_slaves(key,value)
 
     def get(self, key: str) -> dict:
         return self.model.store[key]
@@ -163,9 +204,7 @@ class SingletonPythonDictStorageController:
     def delete(self, key: str):
         if key in self.model.store:
             del self.model.store[key]
-        for s in self.model.slaves:
-            if hasattr(s, 'delete'):
-                s.delete(key)
+        self._delete_slaves(key)
 
     def keys(self, pattern: str):
         return fnmatch.filter(self.model.store.keys(), pattern)
@@ -178,7 +217,7 @@ class SingletonPythonDictStorageController:
         with open(path, "r") as tf:
             self.model.store = json.load(tf)
 
-class SingletonKeyValueStorage:
+class SingletonKeyValueStorage(SingletonStorageController):
 
     def __init__(self) -> None:
         self.python_backend()
@@ -186,16 +225,13 @@ class SingletonKeyValueStorage:
     def python_backend(self):
         self.client = SingletonPythonDictStorageController(SingletonPythonDictStorage())
     
-    if redis_back:
-        def redis_backend(self):
-            self.client = SingletonRedisStorageController(SingletonRedisStorage())
-
     if firestore_back:
-        def firestore_backend(self):
-            self.client = SingletonFirestoreStorageController(SingletonFirestoreStorage())
+        def firestore_backend(self,google_project_id=None,google_firestore_collection=None):
+            self.client = SingletonFirestoreStorageController(SingletonFirestoreStorage(google_project_id,google_firestore_collection))
 
-    def add_slave(self, s):
-        self.client.add_slave(s)
+    if redis_back:
+        def redis_backend(self,redis_URL=None):# 'redis://127.0.0.1:6379'
+            self.client = SingletonRedisStorageController(SingletonRedisStorage(redis_URL))
 
     def exists(self, key: str):
         return self.client.exists(key)
@@ -211,3 +247,9 @@ class SingletonKeyValueStorage:
 
     def keys(self, pattern: str):
         return self.client.keys(pattern)
+
+    def dump(self,json_path):
+        self.client.dump(json_path)
+
+    def load(self,json_path):
+        self.client.load(json_path)
