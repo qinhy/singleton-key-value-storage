1a1efb478d769d9b20ac9b19f3bd844ffb138ee6
HUANGYI QINH <545705507@qq.com>
2024-07-11 10:54:32 +0900

    Update UserModel.py

--- SingletonStorage/UserModel.py
+++ SingletonStorage/UserModel.py
@@ -1,4 +1,51 @@
 
+import base64
+from datetime import datetime
+import io
+import json
+import os
+from typing import Any, List
+from uuid import uuid4
+from zoneinfo import ZoneInfo
+from pydantic import BaseModel, ConfigDict, Field
+
+from .Storages import SingletonKeyValueStorage
+
+def get_current_datetime_with_utc():
+    return datetime.now().replace(tzinfo=ZoneInfo("UTC"))
+
+class Controller4User:
+    class AbstractObjController:
+        def __init__(self, store, model):
+            self.model:Model4User.AbstractObj = model
+            self._store:SingletonKeyValueStorage = store
+
+        def update(self, **kwargs):
+            assert  self.model is not None, 'controller has null model!'
+            for key, value in kwargs.items():
+                if hasattr(self.model, key):
+                    setattr(self.model, key, value)
+            self._update_timestamp()
+            self.store()
+
+        def _update_timestamp(self):
+            assert  self.model is not None, 'controller has null model!'
+            self.model.update_time = get_current_datetime_with_utc()
+            
+        def store(self):
+            self._store._store_obj(self.model)
+            return self
+
+        def delete(self):
+            # self._store.delete_obj(self.model)    
+            self._store.delete(self.model.id)
+            self.model._controller = None
+
+        def update_metadata(self, key, value):
+            updated_metadata = {**self.model.metadata, key: value}
+            self.update(metadata = updated_metadata)
+            return self
+        
 class Model4User:
     class AbstractObj(BaseModel):
         id: str
@@ -7,20 +54,21 @@ class Model4User:
         update_time: datetime = Field(default_factory=get_current_datetime_with_utc)
         status: str = ""
         metadata: dict = {}
+
+        
         model_config = ConfigDict(arbitrary_types_allowed=True)    
-        _controller: Controller4LLM.AbstractObjController = None
+        _controller: Controller4User.AbstractObjController = None
+        def get_controller(self)->Controller4User.AbstractObjController: return self._controller
+        def init_controller(self,store):self._controller = Controller4User.AbstractObjController(store,self)
 
-        def get_controller(self)->Controller4LLM.AbstractObjController: return self._controller
-        def init_controller(self,store):self._controller = Controller4LLM.AbstractObjController(store,self)
-
-    class User:
-        user_id:int = 'auto increatment'
+    class User(AbstractObj):
+        id:int = 'auto increatment'
         email = None
         created_at = None
         updated_at = None
 
-    class App:
-        App_id:int = 'auto increatment'
+    class App(AbstractObj):
+        id:int = 'auto increatment'
         parent_App_id:int = 'auto increatment'
         running_cost = None
         major_name = None
@@ -28,13 +76,13 @@ class Model4User:
         created_at = None
         updated_at = None
 
-    class LicenseApp:
+    class LicenseApp(AbstractObj):
         license_App_id:int = 'auto increatment'
         license_id:int = 'auto increatment'
         App_id:int = 'auto increatment'
 
-    class License:
-        license_id:int = 'auto increatment'
+    class License(AbstractObj):
+        id:int = 'auto increatment'
         user_id:int = 'auto increatment'
         access_token = None
         bought_at = None
@@ -43,8 +91,8 @@ class Model4User:
         max_running_time = None
         updated_at = None
 
-    class AppUsage:
-        usage_id:int = 'auto increatment'
+    class AppUsage(AbstractObj):
+        id:int = 'auto increatment'
         user_id:int = 'auto increatment'
         App_id:int = 'auto increatment'
         license_id:int = 'auto increatment'
