bcdd881fe266ede584ddc75d4372c06083024d6d
HUANGYI QINH <545705507@qq.com>
2024-07-11 10:50:00 +0900

    Update UserModel.py

--- SingletonStorage/UserModel.py
+++ SingletonStorage/UserModel.py
@@ -1,38 +1,53 @@
-class User:
-    user_id:int = 'auto increatment'
-    email = None
-    created_at = None
-    updated_at = None
 
-class App:
-    App_id:int = 'auto increatment'
-    parent_App_id:int = 'auto increatment'
-    running_cost = None
-    major_name = None
-    minor_name = None
-    created_at = None
-    updated_at = None
+class Model4User:
+    class AbstractObj(BaseModel):
+        id: str
+        rank: list = [0]
+        create_time: datetime = Field(default_factory=get_current_datetime_with_utc)
+        update_time: datetime = Field(default_factory=get_current_datetime_with_utc)
+        status: str = ""
+        metadata: dict = {}
+        model_config = ConfigDict(arbitrary_types_allowed=True)    
+        _controller: Controller4LLM.AbstractObjController = None
 
-class LicenseApp:
-    license_App_id:int = 'auto increatment'
-    license_id:int = 'auto increatment'
-    App_id:int = 'auto increatment'
+        def get_controller(self)->Controller4LLM.AbstractObjController: return self._controller
+        def init_controller(self,store):self._controller = Controller4LLM.AbstractObjController(store,self)
 
-class License:
-    license_id:int = 'auto increatment'
-    user_id:int = 'auto increatment'
-    access_token = None
-    bought_at = None
-    expiration_date = None
-    running_time = None
-    max_running_time = None
-    updated_at = None
+    class User:
+        user_id:int = 'auto increatment'
+        email = None
+        created_at = None
+        updated_at = None
 
-class AppUsage:
-    usage_id:int = 'auto increatment'
-    user_id:int = 'auto increatment'
-    App_id:int = 'auto increatment'
-    license_id:int = 'auto increatment'
-    start_time = None
-    end_time = None
-    running_time_cost = None
+    class App:
+        App_id:int = 'auto increatment'
+        parent_App_id:int = 'auto increatment'
+        running_cost = None
+        major_name = None
+        minor_name = None
+        created_at = None
+        updated_at = None
+
+    class LicenseApp:
+        license_App_id:int = 'auto increatment'
+        license_id:int = 'auto increatment'
+        App_id:int = 'auto increatment'
+
+    class License:
+        license_id:int = 'auto increatment'
+        user_id:int = 'auto increatment'
+        access_token = None
+        bought_at = None
+        expiration_date = None
+        running_time = None
+        max_running_time = None
+        updated_at = None
+
+    class AppUsage:
+        usage_id:int = 'auto increatment'
+        user_id:int = 'auto increatment'
+        App_id:int = 'auto increatment'
+        license_id:int = 'auto increatment'
+        start_time = None
+        end_time = None
+        running_time_cost = None
