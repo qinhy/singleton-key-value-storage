79200d2767c780c47dcb36123fa0d45880fb7faf
HUANGYI QINH <545705507@qq.com>
2024-07-11 10:36:53 +0900

    Update UserModel.py

--- SingletonStorage/UserModel.py
+++ SingletonStorage/UserModel.py
@@ -1,12 +1,12 @@
 class User:
-    user_id = None
+    user_id:int = 'auto increatment'
     email = None
     created_at = None
     updated_at = None
 
 class App:
-    App_id = None
-    parent_App_id = None
+    App_id:int = 'auto increatment'
+    parent_App_id:int = 'auto increatment'
     running_cost = None
     major_name = None
     minor_name = None
@@ -14,13 +14,13 @@ class App:
     updated_at = None
 
 class LicenseApp:
-    license_App_id = None
-    license_id = None
-    App_id = None
+    license_App_id:int = 'auto increatment'
+    license_id:int = 'auto increatment'
+    App_id:int = 'auto increatment'
 
 class License:
-    license_id = None
-    user_id = None
+    license_id:int = 'auto increatment'
+    user_id:int = 'auto increatment'
     access_token = None
     bought_at = None
     expiration_date = None
@@ -29,10 +29,10 @@ class License:
     updated_at = None
 
 class AppUsage:
-    usage_id = None
-    user_id = None
-    App_id = None
-    license_id = None
+    usage_id:int = 'auto increatment'
+    user_id:int = 'auto increatment'
+    App_id:int = 'auto increatment'
+    license_id:int = 'auto increatment'
     start_time = None
     end_time = None
     running_time_cost = None
