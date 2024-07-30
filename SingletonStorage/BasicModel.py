eadb22c9dc95928e5579e27d98da090f8f1710dd
koki.chin <545705507@qq.com>
2024-07-30 17:26:49 +0900

    Update BasicModel.py

--- SingletonStorage/BasicModel.py
+++ SingletonStorage/BasicModel.py
@@ -96,8 +96,8 @@ class BasicStore(SingletonKeyValueStorage):
         obj.set_id(id).init_controller(self)
         return obj
     
-    def add_new_obj(self, obj:Model4Basic.AbstractObj, id:str=None):
-        assert obj.get_id() is None
+    def add_new_obj(self, obj:Model4Basic.AbstractObj, id:str=None):        
+        if obj._id is None: raise ValueError(f'obj._id is {obj._id}, must be none')
         id,d = obj.gen_new_id() if id is None else id, obj.model_dump_json_dict()
         self.set(id,d)
         return self._get_as_obj(id,d)
