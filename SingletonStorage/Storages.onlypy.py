0a8a10c2323fb63e33f1d8d08b894b852cb5e935
koki.chin <545705507@qq.com>
2024-09-18 11:30:02 +0900

    Update Storages.onlypy.py

--- SingletonStorage/Storages.onlypy.py
+++ SingletonStorage/Storages.onlypy.py
@@ -1,5 +1,7 @@
-
+# from https://github.com/qinhy/singleton-key-value-storage.git
 import base64
+import hashlib
+import os
 import re
 import sqlite3
 import threading
@@ -9,6 +11,16 @@ import uuid
 import fnmatch
 import json
 import unittest
+import urllib
+import urllib.parse
+from urllib.parse import urlparse
+
+def try_if_error(func):
+    try:
+        func()
+    except Exception as e:
+        print(e)
+        return e
 
 class SingletonStorageController:
     def __init__(self, model):
@@ -31,7 +43,9 @@ class SingletonStorageController:
     def loads(self, json_string=r'{}'): [ self.set(k,v) for k,v in json.loads(json_string).items()]
 
     def dump(self,path):
-        with open(path, "w") as tf: tf.write(self.dumps())
+        data = self.dumps()
+        with open(path, "w") as tf: tf.write(data)
+        return data
 
     def load(self,path):
         with open(path, "r") as tf: self.loads(tf.read())
@@ -40,7 +54,6 @@ class PythonDictStorage:
     def __init__(self):
         self.uuid = uuid.uuid4()
         self.store = {}
-
 class SingletonPythonDictStorage:
     _instance = None
     _meta = {}
@@ -115,7 +128,7 @@ class KeysHistoryController:
     def _str2base64(self,key: str):
         return base64.b64encode(key.encode()).decode()
     def reset(self):
-        self.client.set('_History:',{})
+        self.client = SingletonPythonDictStorageController(PythonDictStorage())        
     def set_history(self,key: str, result:dict):
         if result:
             self.client.set(f'_History:{self._str2base64(key)}',{'result':result})
@@ -132,51 +145,153 @@ class KeysHistoryController:
             if res : self.set_history(key,res)
         return res
 
+class LocalVersionController:
+    def __init__(self,client=None):
+        if client is None:
+            client = SingletonPythonDictStorageController(PythonDictStorage())
+        self.client:SingletonStorageController = client
+        self.client.set(f'_Operations',{'ops':[]})
+    
+    def add_operation(self,operation:tuple,revert:tuple=None):
+        opuuid = str(uuid.uuid4())
+        self.client.set(f'_Operation:{opuuid}',{'forward':operation,'revert':revert})
+        ops = self.client.get(f'_Operations')
+        ops['ops'].append(opuuid)
+        self.client.set(f'_Operations',ops)
+    
+    def revert_one_operation(self,revert_callback:lambda revert:None):
+        ops:list = self.client.get(f'_Operations')['ops']
+        opuuid = ops[-1]
+        op = self.client.get(f'_Operation:{opuuid}')
+        revert = op['revert']        
+        # do revert
+        revert_callback(revert)
+        ops.pop()
+        self.client.set(f'_Operations',{'ops':ops})
+    
+    def get_versions(self):
+        return self.client.get(f'_Operations')['ops']
+
+    def revert_operations_untill(self,opuuid:str,revert_callback:lambda revert:None):
+        ops = [i for i in self.client.get(f'_Operations')['ops']]
+        if opuuid in ops:
+            for i in ops[::-1]:
+                if i==opuuid:break
+                self.revert_one_operation(revert_callback)
+        else:
+            raise ValueError(f'no such version of {opuuid}')
+
 class SingletonKeyValueStorage(SingletonStorageController):
 
     def __init__(self)->None:
         self.conn:SingletonStorageController = None
         self.python_backend()
     
-    def init(self):        
+    def _switch_backend(self,name:str='python',*args,**kwargs):
         self.event_dispa = EventDispatcherController()
         self._hist = KeysHistoryController()
+        self._verc = LocalVersionController()
+        backs={
+            'python':lambda:SingletonPythonDictStorageController(SingletonPythonDictStorage(*args,**kwargs)),
+        }
+        back=backs.get(name.lower(),lambda:None)()
+        if back is None:raise ValueError(f'no back end of {name}, has {list(backs.items())}')
+        return back
     
     def python_backend(self):
-        self.init()
-        self.conn = SingletonPythonDictStorageController(SingletonPythonDictStorage())
+        self.conn = self._switch_backend('python')
     
+    def _print(self,msg):
+        print(f'[{self.__class__.__name__}]: {msg}')
+
     def add_slave(self, slave:object, event_names=['set','delete'])->bool:
-        if slave.__dict__.get('uuid',None) is None: slave.__dict__['uuid'] = uuid.uuid4()
+        if getattr(slave,'uuid',None) is None:
+            try:
+                setattr(slave,'uuid',uuid.uuid4())
+            except Exception:
+                self._print(f'can not set uuid to {slave}. Skip this slave.')
+                return
         for m in event_names:
             if hasattr(slave, m):
-                self.event_dispa.set_event(m,getattr(slave,m),slave.__dict__.get('uuid',None))
+                self.event_dispa.set_event(m,getattr(slave,m),getattr(slave,'uuid'))
+            else:
+                self._print(f'no func of "{m}" in {slave}. Skip it.')
                 
     def delete_slave(self, slave:object)->bool:
-        self.event_dispa.delete_event(slave.__dict__.get('uuid',None))
+        self.event_dispa.delete_event(getattr(slave,'uuid',None))
 
-    def _edit(self,func_name:str, key:str, value:dict=None):
+    def _edit(self,func_name:str, key:str=None, value:dict=None):
+        if func_name not in ['set','delete','clean','load','loads']:
+            self._print(f'no func of "{func_name}". return.')
+            return
         self._hist.reset()
-        func = getattr(self.conn,func_name)
-        args = [key,value] if value else [key] 
+        func = getattr(self.conn, func_name)
+        args = list(filter(lambda x:x is not None, [key,value]))
         res = func(*args)
         self.event_dispa.dispatch(func_name,*args)
         return res
+    
+    def _try_edit_error(self,args):
 
-    def set(self, key: str, value: dict): return self._edit('set',key,value)
-    
-    def delete(self, key: str): return self._edit('delete',key)
-    
-    def exists(self, key: str)->bool: return self._hist.try_history(key, lambda:self.conn.exists(key))
-    
-    def keys(self, regx: str='*')->list[str]: return self._hist.try_history(regx, lambda:self.conn.keys(regx))
+        # do local version controll
+        func = args[0]
+        if func == 'set':
+            func,key,value =args
+            revert = None
+            if self.exists(key):
+                revert = (func,key,self.get(key))
+            else:
+                revert = ('delete',key)
+            self._verc.add_operation(args,revert)
+            
+        elif func == 'delete':
+            func,key = args
+            revert = ('set',key,self.get(key))
+            self._verc.add_operation(args,revert)
 
-    def get(self, key: str)->dict:    return self.conn.get( key)
-    def clean(self):                  return self.conn.clean()
-    def dump(self,json_path):         return self.conn.dump(json_path)
-    def load(self,json_path):         return self.conn.load(json_path)
-    def dumps(self,):                 return self.conn.dumps()
-    def loads(self,json_str):         return self.conn.loads(json_str)
+        elif func in ['clean','load','loads']:
+            revert = ('loads',self.dumps())
+            self._verc.add_operation(args,revert)
+
+        try:
+            self._edit(*args)
+            return True
+        except Exception as e:
+            self._print(e)
+            return False
+    
+    def revert_one_operation(self):
+        self._verc.revert_one_operation(lambda revert:self._edit(*revert))
+
+    def get_current_version(self):
+        vs = self._verc.get_versions()
+        if len(vs)==0:
+            return None
+        return vs[-1]
+
+    def revert_operations_untill(self,opuuid:str):
+        self._verc.revert_operations_untill(opuuid,lambda revert:self._edit(*revert))
+
+    # True False(in error)
+    def set(self, key: str, value: dict):     return self._try_edit_error(('set',key,value))
+    def delete(self, key: str):               return self._try_edit_error(('delete',key))
+    def clean(self):                          return self._try_edit_error(('clean',))
+    def load(self,json_path):                 return self._try_edit_error(('load', json_path))
+    def loads(self,json_str):                 return self._try_edit_error(('loads',json_str))
+    
+    def _try_obj_error(self,func):
+        try:
+            return func()
+        except Exception as e:
+            self._print(e)
+            return None
+    # Object, None(in error)
+    
+    def exists(self, key: str)->bool:         return self._try_obj_error(lambda:self.conn.exists(key))
+    def keys(self, regx: str='*')->list[str]: return self._try_obj_error(lambda:self.conn.keys(regx))
+    def get(self, key: str)->dict:            return self._try_obj_error(lambda:self.conn.get(key))
+    def dumps(self)->str:                     return self._try_obj_error(lambda:self.conn.dumps())
+    def dump(self,json_path)->str:            return self._try_obj_error(lambda:self.conn.dump(json_path))
 
 class Tests(unittest.TestCase):
     def __init__(self,*args,**kwargs)->None:
@@ -197,6 +312,7 @@ class Tests(unittest.TestCase):
         self.test_keys()
         self.test_get_nonexistent()
         self.test_dump_and_load()
+        self.test_version()
         self.test_slaves()
 
     def test_set_and_get(self):
@@ -247,3 +363,15 @@ class Tests(unittest.TestCase):
         self.store.set('gamma', {'info': 'third'})
         self.store.delete('abeta')
         self.assertEqual(json.loads(self.store.dumps()),json.loads(store2.dumps()), "Should return the correct keys and values.")
+
+    def test_version(self):
+        self.store.clean()
+        self.store.set('alpha', {'info': 'first'})
+        data = self.store.dumps()
+        version = self.store.get_current_version()
+
+        self.store.set('abeta', {'info': 'second'})
+        self.store.set('gamma', {'info': 'third'})
+        self.store.revert_operations_untill(version)
+
+        self.assertEqual(json.loads(self.store.dumps()),json.loads(data), "Should return the same keys and values.")
