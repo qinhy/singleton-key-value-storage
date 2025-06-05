03e090a99165a803cf40540e8858fa6e8ab62c3e
koki.chin <545705507@qq.com>
2025-06-05 17:43:56 +0900

    for pip

--- SingletonKeyValueStorage/__init__.py
+++ SingletonKeyValueStorage/__init__.py
@@ -1,2 +1,3 @@
 # from https://github.com/qinhy/singleton-key-value-storage.git
 from .Storages import SingletonKeyValueStorage,Tests
+from .BasicModel import BasicModel,Model4Basic,Controller4Basic
